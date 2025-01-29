dd5b2b77d04e398154000a4ce4255b5b65d41e46
bkirby94 <bkirby@mcad.edu>
2025-01-29 16:30:07 -0700

    Assingment 2.1

---
+++ wk2/2.1/arrays.js
@@ -0,0 +1,17 @@
+//BK Top 5 Movie List
+const myArr = ['X', 'Pearl', 'Scream', 'Raw', 'Bodies, Bodies, Bodies'];
+
+//BK Top 5 Movie List to Console
+console.log(myArr);
+
+//Prompt User for Favorite Movie
+const movie = window.prompt("What is your favorite movie?");
+
+//Add user Movie to Array
+myArr.push(movie);
+
+//BK Top 5 Movie + User Movie to Console
+console.log(myArr);
+
+//Full Movie Displayed to Console
+console.log(myArr.toString());
