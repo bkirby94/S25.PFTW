1b0fed5bbd946c242b38b09116cb6a8d7c3a63e3
bkirby94 <bkirby@mcad.edu>
2025-01-29 16:38:43 -0700

    Update JS

--- wk2/2.1/arrays.js
+++ wk2/2.1/arrays.js
@@ -1,9 +1,6 @@
 //BK Top 5 Movie List
 const myArr = ['X', 'Pearl', 'Scream', 'Raw', 'Bodies, Bodies, Bodies'];
 
-//BK Top 5 Movie List to Console
-console.log(myArr);
-
 //Prompt User for Favorite Movie
 const movie = window.prompt("What is your favorite movie?");
 
@@ -13,5 +10,5 @@ myArr.push(movie);
 //BK Top 5 Movie + User Movie to Console
 console.log(myArr);
 
-//Full Movie Displayed to Console
+//Full Movie Displayed List to Console as String
 console.log(myArr.toString());
