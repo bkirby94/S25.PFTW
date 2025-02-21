87cc799b5323a371c3db7da6e314e79e9bd91bfd
bkirby94 <bkirby@mcad.edu>
2025-02-21 00:17:46 -0500

    5.2 Excercise

--- wk5/5.2/sketch.js
+++ wk5/5.2/sketch.js
@@ -1,7 +1,38 @@
+let squares = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  
+  for (let i = 0; i < 10; i++) {
+    squares.push(new Square(random(width), random(height)));
+  }
 }
 
 function draw() {
-  background(220);
+  background('#E11B8D');
+  
+  for (let square of squares) {
+    square.move();
+    square.show();
+  }
+}
+
+class Square {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 25;
+  }
+
+  move() {
+    this.x = this.x + random(-5, 5);
+    this.y = this.y + random(-5, 5);
+  }
+
+  show() {
+    stroke('#FF98CE');
+    strokeWeight(2)
+    noFill(); 
+    rect(this.x, this.y, this.size, this.size);
+  }
 }
