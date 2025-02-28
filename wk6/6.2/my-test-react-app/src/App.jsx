cc264df815860f8f947a68028ac423ae1a44811b
bkirby94 <bkirby@mcad.edu>
2025-02-28 05:50:53 +0000

    EX 6.2 Update

--- wk6/6.2/my-test-react-app/src/App.jsx
+++ wk6/6.2/my-test-react-app/src/App.jsx
@@ -5,7 +5,7 @@ const birthYear = '1994';
   return (
     <div>
       <h1>Hello World!</h1>
-      <p>This rock is called {planet}.</p>
+      <p>This rock is called {planet}!</p>
       <p>Barbara was born in {birthYear}.</p>
       </div>
   )
