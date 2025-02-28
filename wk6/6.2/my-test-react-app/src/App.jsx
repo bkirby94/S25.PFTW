f5a57f54640ed7c221f896a0a5c12d4df4123376
bkirby94 <bkirby@mcad.edu>
2025-02-28 05:28:13 +0000

    Excercise 6.2

---
+++ wk6/6.2/my-test-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+const planet = 'earth';
+const birthYear = '1994';
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>This rock is called {planet}.</p>
+      <p>Barbara was born in {birthYear}.</p>
+      </div>
+  )
+}
+
+export default App
